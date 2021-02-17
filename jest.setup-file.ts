b72a7f3f9497b88e5a69fa473af7ae5ce9f9029f
Lucas <lucaslacerdacl@gmail.com>
2021-02-17 09:14:31 -0300

    Configs para Jest

---
+++ jest.setup-file.ts
@@ -0,0 +1,6 @@
+import 'reflect-metadata';
+import * as winston from 'winston';
+
+// don't log while running tests
+winston.remove(winston.transports.Console);
+winston.remove(winston.transports.File);
