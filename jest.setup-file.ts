077cdd698b61e51176787934619d776c23fa21f1
Lucas <lucaslacerdacl@gmail.com>
2021-02-17 09:25:17 -0300

    Remoção de comentário

--- jest.setup-file.ts
+++ jest.setup-file.ts
@@ -1,6 +1,5 @@
 import 'reflect-metadata';
 import * as winston from 'winston';
 
-// don't log while running tests
 winston.remove(winston.transports.Console);
 winston.remove(winston.transports.File);
