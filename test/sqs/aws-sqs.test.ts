6b7110200d47ab84e3be09f5cee5ff51447e9278
Lucas <lucaslacerdacl@gmail.com>
2021-03-11 23:45:03 -0300

    Novo recurso para filas fifo.

--- test/sqs/aws-sqs.test.ts
+++ test/sqs/aws-sqs.test.ts
@@ -57,4 +57,57 @@ describe('AWS Sqs', () => {
     });
     expect(mockPromise).toHaveBeenCalled();
   });
+
+  it('Envia mensagem para fila FIFO', async () => {
+    const mockPromise = jest.fn();
+    const mockRequest = new Mock<Request<SQS.SendMessageResult, AWSError>>();
+    const request = mockRequest
+      .setup(instance => instance.promise)
+      .returns(mockPromise)
+      .object();
+
+    const spySendMessage = jest
+      .spyOn(sqs, 'sendMessage')
+      .mockImplementation(() => request);
+
+    const message = {
+      url: 'https://sqs.url.com',
+      module: 'moduleTest',
+      method: 'methodTest',
+      body: {
+        param1: 'param1',
+        param2: 'param2',
+      },
+    };
+
+    const groupId = '264b5233-d600-4298-b8bb-7758e5bcbe7b';
+
+    await awsSqs.sendMessageToQueueFIFO(message, groupId);
+
+    expect(spySendMessage).toHaveBeenCalledWith({
+      DelaySeconds: 0,
+      MessageAttributes: {
+        module: {
+          DataType: 'String',
+          StringValue: message.module,
+        },
+        method: {
+          DataType: 'String',
+          StringValue: message.method,
+        },
+        origin: {
+          DataType: 'String',
+          StringValue: origin,
+        },
+        url: {
+          DataType: 'String',
+          StringValue: message.url,
+        },
+      },
+      MessageBody: JSON.stringify(message.body),
+      QueueUrl: message.url,
+      MessageGroupId: groupId,
+    });
+    expect(mockPromise).toHaveBeenCalled();
+  });
 });
